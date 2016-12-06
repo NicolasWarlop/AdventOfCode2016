8e18bec74d1e6f560c4e03de483a49eacfe725aa
nicolas warlop <nicolas.warlop@gmail.com>
2016-12-06 11:15:55 -0500

    Tweaks to the mostcommon/leastcommon functions to be more readable

--- Day6/AdventDay6.py
+++ Day6/AdventDay6.py
@@ -5,12 +5,12 @@ from collections import Counter
 # Most common logic taken from http://stackoverflow.com/questions/1518522/python-most-common-element-in-a-list
 # Explanation can be found here: https://docs.python.org/3.1/library/collections.html#collections.Counter
 def mostCommon(message_list):
-    data = Counter(message_list)
-    return data.most_common(1)[0][0]
+    data = Counter(message_list).most_common()
+    return data[0][0]
 
 def leastCommon(message_list):
-    data = Counter(message_list)
-    return data.most_common()[:-2:-1][0][0]
+    data = Counter(message_list).most_common()
+    return data[-1][0]
 
 f = open(sys.argv[1], 'r')
 
@@ -20,7 +20,7 @@ message = [list(x.replace('\n','')) for x in message]
 
 # Let's write out our transpose code
 # We have a 8x546 matrix that we want to transpose (to 546x8)
-# for x in 8:
+# for x in 8
 #    #take each of the 546 characters in column x
 #	 for y in 546:
 #		transposed_row += character at [x,y]
@@ -30,7 +30,7 @@ message = [list(x.replace('\n','')) for x in message]
 # for x in range(len(message[0])) # our x values
 # for y in message) # our y values
 # [y[x]] #element from each column, appended into a list
-# [y[x] for y in message for x in range(len(message[0]))]
+# [[y[x] for y in message] for x in range(len(message[0]))]
 
 message_tranpose = [list(y[x] for y in message) for x in range(len(message[0]))]
 
